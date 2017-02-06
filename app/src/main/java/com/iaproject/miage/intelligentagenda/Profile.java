c7a8bf31d51899813ae59c5e5dd477ab9e1904a9
zakaria112012 <m.zak-3@hotmail.com>
2017-02-06 23:43:54 +0100

    80% SignIn

---
+++ app/src/main/java/com/iaproject/miage/intelligentagenda/Profile.java
@@ -0,0 +1,40 @@
+package com.iaproject.miage.intelligentagenda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Profile extends AppCompatActivity {
+
+
+    public FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+       /* if(firebaseAuth.getCurrentUser()==null){
+
+            finish();
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+
+            //dfgbh
+        }*/
+
+        FirebaseUser user=firebaseAuth.getCurrentUser();
+
+
+        TextView TextViewEmail=(TextView) findViewById(R.id.textViewProfile);
+        Button buttonLogout=(Button) findViewById(R.id.buttonLogout);
+
+        TextViewEmail.setText("Bonjour "+user.getEmail());
