a2e34d72932a58e029f78fb7f281bc8babe26203
zakaria112012 <m.zak-3@hotmail.com>
2017-02-22 17:30:17 +0100

    Recuperation password via email 100%

--- app/src/main/java/com/iaproject/miage/intelligentagenda/ResetPasswordActivity.java
+++ app/src/main/java/com/iaproject/miage/intelligentagenda/ResetPasswordActivity.java
@@ -50,7 +50,11 @@ public class ResetPasswordActivity extends AppCompatActivity {
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
-                             
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
 
                             }
                         });
